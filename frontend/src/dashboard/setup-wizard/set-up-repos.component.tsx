e126bb3291c11a07ae5550216e1d61399d942654
Greg Baguley <greg.baguley@gmail.com>
2024-07-08 16:26:51 +1200

    [minimial-wizard] Add minimal wizard text while onboarding

--- frontend/src/dashboard/setup-wizard/set-up-repos.component.tsx
+++ frontend/src/dashboard/setup-wizard/set-up-repos.component.tsx
@@ -11,11 +11,7 @@ export function SetUpRepos(props: Props): JSX.Element {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus nunc in leo pulvinar tristique. Donec purus ligula,
-        egestas accumsan est sed, maximus vehicula odio. Aliquam placerat sollicitudin augue. Donec vel vulputate mi. Nulla luctus
-        neque sed mi sollicitudin aliquam. Donec vel justo quis dui dapibus hendrerit nec eget ipsum. Pellentesque mauris purus,
-        ullamcorper id tristique non, mollis quis odio. Sed id molestie felis, eget rutrum ante. Duis pulvinar lacus purus. Fusce
-        gravida egestas tellus sit amet fermentum. Aenean nec placerat nulla.
+        In order for BuildBeaver to be able to operate against your GitHub repository, you must enable each repository below.
       </p>
       <Repos repoEnabled={refreshSetupStatus} />
     </>
